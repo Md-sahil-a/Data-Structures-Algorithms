9ad80535845e8b7232fc7b7b2c6c155658567a22
Muhammad Sahil Amaan <77974484+Md-sahil-a@users.noreply.github.com>
2023-02-18 10:13:26 +0530

    Time: 71 ms (35.98%), Space: 42.2 MB (31.60%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    let target = [];
+    
+    for(let i=0; i<nums.length; i++)
+        {
+            target.splice(index[i], 0, nums[i]);
+        }
+    return target;
+    
+};
