0713a061ea917b4338af608af476008e5da78442
Muhammad Sahil Amaan <77974484+Md-sahil-a@users.noreply.github.com>
2023-02-27 12:43:57 +0530

    Time: 60 ms (86.90%), Space: 42.4 MB (39.20%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let res = [];
+    let sum = 0;
+    for(let i=0; i<nums.length; i++)
+        {
+            sum +=nums[i];
+        res.push(sum);
+        }
+    return res;
+};
