e703a88e73c22fed9f03f14981d161c63cbdc52d
Muhammad Sahil Amaan <77974484+Md-sahil-a@users.noreply.github.com>
2023-03-04 12:28:13 +0530

    Time: 55 ms (90.86%), Space: 41.4 MB (94.76%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+    if(n%2==0)
+        {
+            return n;
+        }else{
+            return n*2
+        }
+    
+    
+};
