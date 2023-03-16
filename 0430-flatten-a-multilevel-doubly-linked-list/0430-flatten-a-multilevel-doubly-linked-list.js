3953726e9c263d1ac9da12904130638849e550c1
Muhammad Sahil Amaan <77974484+Md-sahil-a@users.noreply.github.com>
2023-03-16 21:44:43 +0530

    Time: 47 ms (99.27%), Space: 43.6 MB (62.10%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.js
@@ -0,0 +1,41 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function(head) {
+     if (!head) return head;
+
+  let currentNode = head;
+  while (currentNode !== null) {
+    if (currentNode.child === null) {
+      currentNode = currentNode.next;
+    } else {
+      let tail = currentNode.child;
+      while (tail.next !== null) {
+        tail = tail.next;
+      }
+
+      tail.next = currentNode.next;
+      if (tail.next !== null) {
+        tail.next.prev = tail;
+      }
+
+      currentNode.next = currentNode.child;
+      currentNode.next.prev = currentNode;
+      currentNode.child = null;
+    }
+  }
+
+  return head;
+    
+};
