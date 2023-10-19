5a8b9a3438a685b37924826b08d2fae46b8f01c7
Muhammad Sahil Amaan <77974484+Md-sahil-a@users.noreply.github.com>
2023-10-19 23:29:21 +0530

    Time: 230 ms (5.94%), Space: 51.4 MB (6.42%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -7,16 +7,36 @@ var findNumbers = function(nums) {
 let c = 0;
 
 
+
+
+
+
+let fun = (a) =>{
+let b = '';
+  b += a;
+  return b;
+}
+
+let splitc = (b) =>{
+  let arr = [];
+  for(let i=0;i<b.length; i++)
+    {
+      arr.push(b[i]);
+    }
+  return arr;
+}
+
 for(let i =0; i<nums.length; i++)
   {
-
-    let len = nums[i].toString().split('').length
-    if(len %2==0)
+    let str = fun(nums[i]);
+    console.log(str)
+    let len = splitc(str);
+    console.log(len);
+    if(len.length%2==0)
     {
       c++;
     }
-    
-    
+
   }
 return c; 
     
