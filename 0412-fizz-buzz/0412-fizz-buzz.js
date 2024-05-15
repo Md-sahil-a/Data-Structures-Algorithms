6794344db630adf37bf3ec9e3813a4d8b34d2967
Muhammad Sahil Amaan <mdsahilamaan2017@gmail.com>
2024-05-15 22:51:04 +0530

    re-code
    Co-Authored-By: ShaheelByteCrafters <152173719+shaheelbytecrafters@users.noreply.github.com>

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -2,24 +2,24 @@
  * @param {number} n
  * @return {string[]}
  */
-// var fizzBuzz = function(n) {
-//     let res = [];
-//     for(let i=1; i<=n; i++)
-//         {
-//             if(i%3==0 && i%5==0)
-//                 {
-//                     res.push("FizzBuzz");
-//                 }else if(i%3==0)
-//                     {
-//                         res.push("Fizz");
-//                     }else if(i%5==0){
-//                         res.push("Buzz");
-//                     }
-//             else{
-//                 res.push(String(i));
-//             }
+var fizzBuzz = function(n) {
+    let res = [];
+    for(let i=1; i<=n; i++)
+        {
+            if(i%3==0 && i%5==0)
+                {
+                    res.push("FizzBuzz");
+                }else if(i%3==0)
+                    {
+                        res.push("Fizz");
+                    }else if(i%5==0){
+                        res.push("Buzz");
+                    }
+            else{
+                res.push(String(i));
+            }
 
-//         }
-//     return res;
+        }
+    return res;
 
-// };
+};
